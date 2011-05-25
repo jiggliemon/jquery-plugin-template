381761a1d2b43770fd527fe738ac3ca85b467364
chad d <chad@agroism.com>
2011-05-24 18:01:25 -0700

    first commit

---
+++ js/_Plugin_.js
@@ -0,0 +1,55 @@
+/*
+    replace _plugin_ 
+      - the lowercase name of your plugin
+ 
+    replace _Plugin_
+      - the uppercase name of your plugin (Contructor)
+*/
+var namespace = jQuery || window;
+ 
+(function (jq,namespace) {
+    var options = {
+        option: 'blah',
+        evented: true,
+        debug: false
+    };
+        
+    /* _Plugin_ v0.9 */
+    var _Plugin_ = namespace._Plugin_ = function (elements, opts) {
+        
+        // the _plugin_ config based on passed in options
+        if(opts){
+            options = jq.extend({}, opts || {});
+            if(options.events &amp;&amp; namespace.Events){
+                jq.extend( this, new Events(), options.events ); 
+            }
+        }
+        
+        // initiate the _Plugin_
+        return (this.initialize)?this.initialize(elements):this;
+    };
+    
+    _Plugin_.prototype = {
+        
+        initialize : function (elements) {
+            
+            // Get the _plugin_ element(s)
+            this.elements = jq(elements)
+           ,count = this.elements.length;
+            
+            // Do some processing to the element(s)
+            while(count--){
+                var element = this.elements[count] = jq(this.elements[count]);
+                element.data("_plugin_",this);
+            };
+ 
+            return this;
+        }
+    };
+ 
+    // jQuery bridge
+    jq.fn._plugin_ = function (options) {
+        return new _Plugin_(this, options).elements;
+    };
+ 
+})(jQuery,namespace);
